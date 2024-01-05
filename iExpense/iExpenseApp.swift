359c990622094461cefa6283c60939b2d8a982ce
Jack Lingle <captainjack0818@gmail.com>
2024-01-05 17:12:54 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jack Lingle on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
